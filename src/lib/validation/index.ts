6ec65111455e5edbe51de2d8c8fb70f4abb1b010
Blankz1035 <andrew.blankley@hotmail.com>
2024-07-01 16:14:47 +0100

    add loader and final styling of signup

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z from 'zod'
+
+export const SignUpValidation = z.object({
+    name: z.string().min(2, {message: "Name too Short"}), 
+    username: z.string().min(2, {message: "Username too Short"}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 Characters"})
+  })
+  
