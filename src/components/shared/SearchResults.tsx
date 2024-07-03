20a03e741c400556d8bfb0c60cabb66aadbbfbed
Blankz1035 <andrew.blankley@hotmail.com>
2024-07-03 17:50:27 +0100

    fix query bug to invalidate search term and create search results page layout

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,8 +1,26 @@
+import { Models } from 'appwrite';
 import React from 'react'
+import Loader from './loader';
+import GridPostList from './GridPostList';
 
-const SearchResults = () => {
+type SearchResultsProps = {
+    isSearchFetching: boolean;
+    searchedPosts: Models.Document[]
+}
+const SearchResults = ({isSearchFetching, searchedPosts} : SearchResultsProps) => {
+    if(isSearchFetching){
+        return (
+            <Loader />
+        )
+    }
+    if(searchedPosts && searchedPosts.documents.length > 0){
+        return (
+            <GridPostList posts={searchedPosts.documents} />
+        )
+    }
+
   return (
-    <div>SearchResults</div>
+    <p className='text-light-4 mt-10 w-full text-center'>No results found</p>
   )
 }
 
